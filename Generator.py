6916b3b238d595c8b7c263f15891197776d86125
NickaStorm <nicka@ai-engineered.com>
2021-05-17 09:08:09 -0600

    Added comments for the school project

--- Generator.py
+++ Generator.py
@@ -1,5 +1,7 @@
 import random
 
+
+# this will be our list of all of wanted characters
 allcharlist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
@@ -8,13 +10,20 @@ allcharlist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
                "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "|", ";",
                ":", "'", ",", ".", "<", ">", "?", "/"]
 
+# this list helps us with generating the combination of words later on
+numberlist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# this function will help us with user readability and accessiblity
 def syntaxerror():
     print("* * * * * * * * * * * * * * * * * * * * * * *")
     print("Please try to answer with one of the prompts.")
 
+# this breaks the blocks of text up for readability
 def astline():
     print("* * * * * * * * * * * * * * * * * * * * * * *\n")
 
+# this function asks the user if they want to repeat the script
 def askagain():
     useagain = input("Would you like to use this script again?\n[Yes/No]\n>")
     if useagain.lower() == "yes":
@@ -27,6 +36,7 @@ def askagain():
         astline()
         askagain()
 
+# the starting script that gathers most of the needed input and information
 def welcome():
     astline()
     choice = input("Do you want to: \nGenerate a password \nOR \nCheck the security of a existing password \nAnswers: [Generate/Check] \n>")
@@ -38,7 +48,7 @@ def welcome():
             generator(input("What security level would you like your password? \n[Easy, Medium, Hard]\n>"))
         elif word_or_rand.lower() == "word":
             astline()
-            print("This mode is still being implemented")
+            word_generator()
         else:
             syntaxerror()
             welcome()
@@ -50,6 +60,19 @@ def welcome():
         syntaxerror()
         welcome()
 
+# this function takes the words from a txt file, and concatenate them
+def word_generator():
+    word_password = ""
+    with open("1000mostcommonwords", "r") as text_file:
+        text_read = text_file.read()
+        seperate_lines = text_read.split()
+        for num in range(0, random.randint(3, 6)):
+            word_password = word_password + seperate_lines[random.randint(0, len(seperate_lines) - 1)]
+        print("Your password is " + word_password)
+        astline()
+        askagain()
+
+# this function just takes random letters from the list, and adds them together
 def simple_generator(difficulty, min, max):
     password = ""
     astline()
@@ -59,6 +82,7 @@ def simple_generator(difficulty, min, max):
     print(password)
     askagain()
 
+# this function established the difficulty of password the user wants, and relays this info to the function above
 def generator(security_lvl):
     if security_lvl.lower() == "easy":
         simple_generator(security_lvl.lower(), 7, 9)
@@ -70,6 +94,7 @@ def generator(security_lvl):
         syntaxerror()
         generator(input("What security level would you like your password? \n[Easy, Medium, Hard]\n>"))
 
+# this function works by checking the occurence of each letter, and the length of the password, and assigns it to a number value
 def check(input_password):
     password_value = len(input_password) * 3
     for letter in input_password:
@@ -90,4 +115,6 @@ def check(input_password):
         print("This is a great password\nJust remember it, so you don't forget it.")
     askagain()
 
+
+# this starts the entire script
 welcome()
